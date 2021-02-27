75c02486f7a00a70040cced6046fe6fb6eb3390d
Gaurav <gauravguglani1@gmail.com>
2021-02-27 23:37:55 +0000

    Commit from gaurav

--- src/test/java/Academy/validateTitle.java
+++ src/test/java/Academy/validateTitle.java
@@ -38,7 +38,7 @@ public class validateTitle extends base{
 		// creating object to that class and invoke methods of it
 		LandingPage l=new LandingPage(driver);
 		//compare the text from the browser with actual text.- Error..
-		Assert.assertEquals(l.getTitle().getText(), "FEATURED COURSES");
+		Assert.assertEquals(l.getTitle().getText(), "FFEATURED COURSES");
 		 log.info("Successfully validated Text message");
 		 System.out.println("Test completed");
 		 
